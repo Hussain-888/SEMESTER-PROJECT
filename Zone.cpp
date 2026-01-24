11b871bb32cfa02951c75fe231a3822a837d0ef8
Hussain-888! <hussainsubhani693@gmail.com>
2026-01-24 18:14:58 +0500

    zone c++ part1

---
+++ Zone.cpp
@@ -0,0 +1,35 @@
+#include "Zone.h"
+#include "ParkingArea.h"
+
+Zone::Zone(int id, int maxAreas) : zoneId(id), areaCount(0), areaCapacity(maxAreas) {
+    parkingAreas = new ParkingArea*[maxAreas];
+    for (int i = 0; i < maxAreas; i++) {
+        parkingAreas[i] = nullptr;
+    }
+}
+
+Zone::~Zone() {
+    for (int i = 0; i < areaCount; i++) {
+        delete parkingAreas[i];
+    }
+    delete[] parkingAreas;
+}
+
+int Zone::getZoneId() const {
+    return zoneId;
+}
+
+bool Zone::addParkingArea(ParkingArea* area) {
+    if (areaCount < areaCapacity) {
+        parkingAreas[areaCount++] = area;
+        return true;
+    }
+    return false;
+}
+
+ParkingArea* Zone::getParkingArea(int index) const {
+    if (index >= 0 && index < areaCount) {
+        return parkingAreas[index];
+    }
+    return nullptr;
+}
